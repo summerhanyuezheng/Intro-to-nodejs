9473a177c7e455e974e4d250470070bf79e84e35
Summer Zheng <summersleepy69@gmail.com>
2023-04-25 17:27:22 -0700

    Add files via upload

---
+++ intro-to-node/index.js
@@ -0,0 +1,11 @@
+const superheroes = require('superheroes')
+superheroes.all
+MyHeroName = superheroes.random()
+console.log(MyHeroName)
+
+
+
+const supervillains = require('supervillains');
+supervillains.all;
+MyVilllainName = supervillains.random();
+console.log(MyVilllainName)
